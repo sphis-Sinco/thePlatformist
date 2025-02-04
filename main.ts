5207ad6ab5d2b5680d9b70c38157cf4493c2aac0
Sinco <sinconsistencia@gmail.com>
2025-02-04 16:35:08 -0600

    startScreen function withg the startScreen

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (gameState == 0) {
+    	
+    }
+})
+function startScreen () {
+    scene.setBackgroundColor(15)
+    scene.setBackgroundImage(assets.image`startscreen`)
+    gameState = 0
+}
+let gameState = 0
+startScreen()
